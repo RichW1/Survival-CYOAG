43d7457964d1f916698920e0546105365f699a8c
RichW1 <richpwager@gmail.com>
2019-11-13 20:49:54 -0700

    Changes more

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Events from './events';
+import Scenarios from './scenarios';
+
+function App()
+{
+    return(
+        <div>
+        <Events />
+        <Scenarios />
+        </div>
+    );
+};
+
+export default App;
---
+++ my-app/src/events.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Events()
+{
+    return;
+};
+
+export default Events;
